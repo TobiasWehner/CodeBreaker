f20bbb1a89f8c2449c8da330709df196156ddfcf
TobiasWehner <71069013+TobiasWehner@users.noreply.github.com>
2020-10-12 13:04:17 +0200

    Add files via upload

---
+++ CodeBreakerV6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace CodeBreakerV6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, 1000);
+            string Input;
+            int number = 0;
+            bool isNumber = false;
+            bool isEqual = false;
+            int i = 1;
+
+            while (isEqual == false)
+            {
+                while (isNumber == false)
+                {
+                    WriteLine("Dein {0}. Versuch Spieler 2, gebe bitte eine Zahl ein!", i);
+                    Input = ReadLine();
+                    isNumber = Int32.TryParse(Input, out number);
+                    Clear();
+                }
+
+                if (number == secretNumber)
+                {
+                    WriteLine("Die Zahl ist richtig");
+                    isEqual = true;
+                }
+                else if (number > secretNumber)
+                {
+                    WriteLine("Die eingegebene Zahl ist zu groß");
+                }
+                else if (number < secretNumber)
+                {
+                    WriteLine("Die eingegebene Zahl ist zu klein");
+                }
+                else
+                {
+                    WriteLine("Die Zahl nicht richtig");
+                }
+                Thread.Sleep(1020);
+                Clear();
+                isNumber = false;
+                i++;
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ CodeBreakerV8/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace CodeBreakerV8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 2147483647;
+            int average = (min + max) / 2;
+
+            int minValue = 0;
+            int maxValue = -1;
+            Random rnd = new Random();
+            int secretNumber = 0;
+            string secretInput = "";
+            string Input;
+            int number = 0;
+            bool isNumber = false;
+            bool isEqual = false;
+            int i = 1;
+            while (minValue > maxValue)
+            {
+                while (isNumber == false)
+                {
+                    WriteLine("Spieler 1, gebe das untere Zahlen limit ein!", i);
+                    secretInput = ReadLine();
+                    isNumber = Int32.TryParse(secretInput, out minValue);
+                    Clear();
+                }
+                isNumber = false;
+                while (isNumber == false)
+                {
+                    WriteLine("Spieler 1, gebe das obere Zahlen limit ein!", i);
+                    secretInput = ReadLine();
+                    isNumber = Int32.TryParse(secretInput, out maxValue);
+                    Clear();
+                }
+                isNumber = false;
+            }
+            try
+            {
+                secretNumber = rnd.Next(minValue, maxValue + 1);
+            }
+            catch
+            {
+                secretNumber = rnd.Next(minValue, maxValue);//um 2147483647 nicht zu zulassen
+            }
+
+            while (isEqual == false)
+            {
+                if (average == secretNumber)
+                {
+                    isEqual = true;
+                }
+                else if (average > secretNumber)
+                {
+                    max = average;
+                    average = (min + max) / 2;
+                }
+                else if (average < secretNumber)
+                {
+                    min = average + 1;
+                    average = (min + max) / 2;
+                }
+                isNumber = false;
+                i++;
+            }
+            WriteLine("Die Zufallszahl war {0}.", secretNumber);
+            WriteLine("Die errechnete Zahl ist {0}.", average);
+        }
+    }
+}
